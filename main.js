43c3cb9bbf092786d1e7ccd95d3b5d14554a42fa
38amit <75780317+38amit@users.noreply.github.com>
2021-10-15 14:44:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song = "";
+
+function preload() {
+    song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(450, 450);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 450, 450);
+}
+
+function play() {
+    song.play();
+}
